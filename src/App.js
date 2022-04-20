b7c07b865d7801180e6a341c9147da528d740034
loureiro-lucas <lucas-loureiro@outlook.com>
2022-04-19 22:21:52 -0300

    deploy to github pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Header from './components/Header'
 import HeroesList from './components/HeroesList'
 import CssBaseline from '@mui/material/CssBaseline';
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
       <CssBaseline />
       <Header />
-      <HeroesList />
+      <Routes>
+        <Route path='/' element={ <HeroesList /> } />
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider store={ store }>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
