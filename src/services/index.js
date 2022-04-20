9fc6b1a35e77b09f1d0b24f6497d2a9aa8e5f667
loureiro-lucas <lucas-loureiro@outlook.com>
2022-04-19 22:26:37 -0300

    deploy to github pages

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
 export const fetchHeroes = () => {
-  return fetch('http://homologacao3.azapfy.com.br/api/ps/metahumans')
+  return fetch('https://homologacao3.azapfy.com.br/api/ps/metahumans')
     .then((response) => response.json());
 }
